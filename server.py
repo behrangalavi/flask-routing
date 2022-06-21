91a2b9152eb53bdec54c816088741d42bb475233
Behrang Alavi <90796287+behrangalavi@users.noreply.github.com>
2022-06-21 13:24:23 +0200

    Update server.py

--- server.py
+++ server.py
@@ -5,5 +5,15 @@ server = Flask(__name__)
 def hello():
     return "Hello World!"
 
+
+@server.route("/visualizePrediction", methods=["GET","POST"])
+def visualizePrediction():
+    if request.method == "GET":
+        return "found visualizePrediction"
+    elif request.method == "POST":
+        requestJson = request.get_json()
+        return jsonify(requestJson["prediction"])
+
+
 if __name__ == "__main__":
    server.run(host='0.0.0.0', port=1337)

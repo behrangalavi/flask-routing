1d918cf69eb42271cd027ad1cd79d944c72cb1bc
Behrang Alavi <90796287+behrangalavi@users.noreply.github.com>
2022-06-21 16:32:04 +0200

    Update server.py

--- server.py
+++ server.py
@@ -1,12 +1,38 @@
-from flask import Flask, request, jsonify
+from flask import Flask
+from flask import request
+from flask import render_template
+from flask.json import jsonify
+
 server = Flask(__name__)
+
 
 @server.route("/")
 def hello():
-    return "Hello World!"
+    encoded_plots = [
+        generate_view("monday", get_mock_data_server('testoutput2.json')),
+        generate_view("tuesday", get_mock_data_server('testoutput2.json')),
+        generate_view("wednesday", get_mock_data_server('testoutput2.json')),
+        generate_view("thursday", get_mock_data_server('testoutput2.json')),
+        generate_view("friday", get_mock_data_server('testoutput2.json')),
+        generate_view("saturday", get_mock_data_server('testoutput2.json')),
+        generate_view("combined", get_mock_data_server('testoutput2.json'))
+    ]
+
+    dropdown_values = [
+        "Montag",
+        "Dienstag",
+        "Mittwoch",
+        "Donnerstag",
+        "Freitag",
+        "Samstag",
+        "Wochenansicht"
+    ]
+
+    return render_template("marktseite.html", list=encoded_plots, labels=dropdown_values)
 
 
-@server.route("/visualizePrediction", methods=["GET","POST"])
+
+@server.route("/visualizePrediction", methods=["GET", "POST"])
 def visualizePrediction():
     if request.method == "GET":
         return "found visualizePrediction %s" % (request.method)
@@ -14,6 +40,17 @@ def visualizePrediction():
         requestJson = request.get_json()
         return jsonify(requestJson["prediction"])
 
+@server.route("/visualizePredictionForWholeWeek", methods=["GET", "POST"])
+def visualizePredictionForWholeWeek():
+    if request.method == "GET":
+        return "found visualizePrediction %s" % (request.method)
+    else:
+        generate_view("combined", request.get_json)
+        requestJson = request.get_json()
+        return jsonify(requestJson["prediction"])
+
+
 
 if __name__ == "__main__":
-    server.run(host='0.0.0.0', port=1337)
+   server.run(host='0.0.0.0', port=1337)
+

9ad89d94835dbc678f776f83a8965fd695e3d05d
Behrang Alavi <90796287+behrangalavi@users.noreply.github.com>
2022-06-21 16:14:51 +0200

    Update server.py

--- server.py
+++ server.py
@@ -1,44 +1,14 @@
-from flask import Flask
-from flask import request, jsonify
-from flask import render_template
-
-server = Flask(name)
-
+from flask import Flask, request, jsonify
+server = Flask(__name__)
 
 @server.route("/")
 def hello():
-    encoded_plots = [
-        generate_view("monday", get_mock_data()),
-        generate_view("tuesday", get_mock_data()),
-        generate_view("wednesday", get_mock_data()),
-        generate_view("thursday", get_mock_data()),
-        generate_view("friday", get_mock_data()),
-        generate_view("saturday", get_mock_data()),
-        generate_view("combined", get_mock_data())
-    ]
+    return "Hello World!"
 
-    dropdown_values = [
-        "Montag",
-        "Dienstag",
-        "Mittwoch",
-        "Donnerstag",
-        "Freitag",
-        "Samstag",
-        "Wochenansicht"
-    ]
-
-    return render_template("marktseite.html", list=encoded_plots, labels=dropdown_values)
-
-
-if name == "main":
-   server.run(host='0.0.0.0', port=1337)
-
-@server.route("/visualizePredictionForSingleDay", methods=["GET", "POST"])
-def visualizePredictionForSingleDay():
+if __name__ == "__main__":
     server.run(host='0.0.0.0', port=1337)
 
-
-@server.route("/visualizePrediction", methods=["GET", "POST"])
+@server.route("/visualizePrediction", methods=["GET","POST"])
 def visualizePrediction():
     if request.method == "GET":
         return "found visualizePrediction %s" % (request.method)
@@ -46,11 +16,3 @@ def visualizePrediction():
         requestJson = request.get_json()
         return jsonify(requestJson["prediction"])
 
-@server.route("/visualizePredictionForWholeWeek", methods=["GET", "POST"])
-def visualizePredictionForWholeWeek():
-    if request.method == "GET":
-        return "found visualizePrediction %s" % (request.method)
-    else:
-        generate_view("combined", request.get_json)
-        requestJson = request.get_json()
-        return jsonify(requestJson["prediction"])
